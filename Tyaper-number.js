88a670313684e1f8f96bc06681af06e231bd135f
Jonathanclps <134884017+Jonathanclps@users.noreply.github.com>
2023-07-12 18:51:43 -0300

    Add files via upload

---
+++ Tyaper-number.js
@@ -0,0 +1,10 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const opercaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(opercaoMatematica);
---
+++ tyaper-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo";
+const texto2 = 'Olá, mundo';
+const senha = "superSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Jonathan";
+
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
