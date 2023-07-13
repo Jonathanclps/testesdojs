3d0776c94cbbd6440230a6076b22dd131b2f979e
Jonathanclps <134884017+Jonathanclps@users.noreply.github.com>
2023-07-13 19:39:01 -0300

    Add files via upload

---
+++ conversos.js
@@ -0,0 +1,13 @@
+// tipo de dado 
+// boolean
+
+//conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+//Number()
+//String()
+
+console.log(numero + Number(numeroString))
+
+
