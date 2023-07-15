a94296f1214676bb606939b1e8a2bbf337056445
Jonathanclps <134884017+Jonathanclps@users.noreply.github.com>
2023-07-15 16:00:34 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+//let x = "";
+//console.log(x)
+//x = "oi";
+
+//DECLARAÇÃO DE FUNÇÃO
+
+//1) declara a função 
+
+                      //string deixa de ser string  e passsa a ser 2 >= valor de soma
+function imprimeTexto (texto) {
+    console.log(texto)
+}
+
+//2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+//imprimeTexto("Outro texto")
+
+//tres formas de escrever funções 
+
+function soma() {
+    //outros codigos 
+    //return tem que ser o ultimo codigo
+    return  2 + 2;
+}
+
+//console.log(soma())
---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Jon";
+const idade = 2023 - 2005;
+const cidadeDeNascimento = "Porto Alegre";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci em " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
+
